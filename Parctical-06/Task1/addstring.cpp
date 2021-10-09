22688fceb29bac08430758fd3fee5a82a1a30b19
HARSHIT-BHAINSORA <90299672+HARSHIT-BHAINSORA@users.noreply.github.com>
2021-10-09 10:53:19 +0530

    Add files via upload

---
+++ Parctical-06/Task1/addstring.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+	string d = "Hello";
+	string e = "Everyone";
+	
+	cout<<"Sum of two string : "<<d+e<<endl;
+	return 0;
+}
---
+++ Parctical-06/Task1/intfloat.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+	int a = 30;
+	float c = 3.9;
+	cout<<"Sum of intger and float :" << a+c <<endl;
+	return 0;
+}
