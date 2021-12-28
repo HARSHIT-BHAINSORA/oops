9d095f8671f1379ff679c6ad294793c86066bd74
HARSHIT-BHAINSORA <90299672+HARSHIT-BHAINSORA@users.noreply.github.com>
2021-12-28 13:46:01 +0530

    task1

---
+++ Practical-14/task1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+
+int main()
+{
+    ifstream myfile;
+    myfile.open("Demofile.txt");
+    myfile>>data;'
+    cout<<data<< endl;
+    return 0;
+}
